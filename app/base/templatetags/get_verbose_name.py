873a352f29e8c6b25291d5a5acbc9a3381e2bd41
Jhonatan Patrocinio <jhonatan.patrocinio@sou.ufac.br>
2023-10-29 19:48:15 -0500

    saving

--- app/base/templatetags/get_verbose_name.py
+++ app/base/templatetags/get_verbose_name.py
@@ -7,4 +7,5 @@ def get_verbose_field_name(instance, field_name):
     """
     Returns verbose_name for a field.
     """
-    return instance._meta.get_field(field_name).verbose_name.title()
+    return ''
+    # return instance._meta.get_field(field_name).verbose_name.title()
